a4b675eb4c277e6957d31144bf5acbf09c0bc0d5
Drakaryss <dasdasd@sdasd.cl>
2019-04-23 18:27:08 -0400

    agregand cambios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FrameVideo from '../src/components/FrameVideo';
 import LogoPagina from '../src/components/LogoPagina';
